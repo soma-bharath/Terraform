48e8ea285f54e6192ebe226c15feaab1a0b88131
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-12 07:56:52 +0530

    Create kubesetup.sh

---
+++ kubesetup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+EOF
+sudo yum install -y kubectl
+sudo aws eks --region us-west-2 update-kubeconfig --name test-eks-cluster
