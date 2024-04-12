b4dd241e79afaa0cb126674293fa04e59aa7148e
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2024-04-12 08:20:42 +0530

    Update kubesetup.sh

--- kubesetup.sh
+++ kubesetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -8,4 +9,8 @@ gpgcheck=1
 gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
 EOF
 sudo yum install -y kubectl
-sudo aws eks --region us-west-2 update-kubeconfig --name test-eks-cluster
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker pull python
+sudo cp ${path.module}/
